26138218d051f1be8b3632a3ec146c5895249569
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2022-10-31 10:59:55 -0500

    commit tree 527441e264fab636376498b914054d42ae86f58d

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { image } from "../data/data";
+import { image as IMG} from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+            <h2>About Me</h2>
+            <p>cats</p>
+            <img src={ IMG } alt="I made this"></img>
+  </div>;
 }
 
 export default About;
